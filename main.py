8f0d3c934c7f10d3baaa641ea798eac487873f42
Jasper Zanjani <jasper.zanjani@gmail.com>
2026-04-08 01:00:44 -0500

    scale

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ from gi.repository import Gtk, Adw, Gio, Gdk, GObject
 from wonderwords import RandomWord
 from random import randrange
 
-# data = [
-#     {"name": "USS Enterprise", "image": "enterprise.png", "registry": "NCC-1701"},
-#     {"name": "USS Reliant", "image": "reliant.png", "registry": "NCC-1864"},
-#     {"name": "USS Defiant", "image": "defiant.png", "registry": "NX-74205"},
-# ]
-
 class Starship(GObject.Object):
     def __init__(self, name, image, registry):
         super().__init__()
@@ -41,11 +35,15 @@ class CustomAvatar(Adw.Bin):
 
 class MyApp(Adw.Application):
     random_word = RandomWord()
+    builder = Gtk.Builder()
+
+    def on_button_clicked(self, button):
+        self.get_data()
+
     def do_activate(self):
-        builder = Gtk.Builder()
-        builder.add_from_file("main.ui")
+        self.builder.add_from_file("main.ui")
         if not self.props.active_window:
-            win = builder.get_object("main")
+            win = self.builder.get_object("main")
 
         win.set_application(self)
         win.present()
@@ -62,22 +60,28 @@ class MyApp(Adw.Application):
             custom_avatar.name.set_label(model_item.name)
             custom_avatar.registry.set_label(model_item.registry)
                
-        gridview = builder.get_object("gridview")
+        self.gridview = self.builder.get_object("gridview")
         factory = Gtk.SignalListItemFactory()
         factory.connect("setup", on_factory_setup)
         factory.connect("bind", on_factory_bind)
-        gridview.set_factory(factory)
+        self.gridview.set_factory(factory)
+        self.get_data()
 
+        button = self.builder.get_object("button")
+        button.connect("clicked", self.on_button_clicked)
+
+    def get_data(self):
+        scale = self.builder.get_object("ship_number")
+        scale_adjustment = scale.get_adjustment()
+        ship_number = int(scale_adjustment.get_value());
+        print(f"ship number: {ship_number}")
         data_model = Gio.ListStore(item_type=Starship)
-        # for item in data:
-        #     gobject = Starship(name=item['name'], image=item['image'], registry=item['registry'])
-        #     data_model.append(gobject)
-        for i in range(100):
+        for i in range(ship_number):
             name = self.random_word.word(include_parts_of_speech=['adjectives'])
             gobject = Starship(name=f"USS {name.title()}", image="enterprise-stripped.png", registry=f"NCC-{randrange(10000)}")
             data_model.append(gobject)
         selection_model = Gtk.SingleSelection.new(data_model)
-        gridview.set_model(selection_model)
+        self.gridview.set_model(selection_model)
 
 if __name__ == '__main__':
     app = MyApp()
